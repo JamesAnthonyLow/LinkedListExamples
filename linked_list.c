9a2cbc3370612d8152e671798f29819e977ece34
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-10-19 17:27:57 -0400

    appendToTail

--- linked_list.c
+++ linked_list.c
@@ -17,3 +17,37 @@ struct Node * newNode(int data)
 
   return n;
 }
+
+struct Node * appendToTail(struct Node * head, struct Node * n)
+{
+  struct Node * curr;
+
+  if( ! head )
+    return head = n;
+
+  curr = head;
+  while( curr->next )
+    curr = curr->next;
+  curr->next = n;
+  return head;
+}
+
+void printList(struct Node * head)
+{
+  if( ! head ) {
+    printf("NULL\n");
+    return;
+  }
+  printf("Node( %d ) -> ", head->data);
+  printList( head->next );
+}
+
+int main(){
+  struct Node * head;
+
+  head = appendToTail(NULL, newNode(1));
+  head = appendToTail(head, newNode(2));
+  head = appendToTail(head, newNode(3));
+  head = appendToTail(head, newNode(4));
+  printList(head);
+}
--- linked_list.rb
+++ linked_list.rb
@@ -5,3 +5,28 @@ class Node
     @next = nil
   end
 end
+
+class LinkedList
+  def initialize
+    @head = nil
+  end
+  def appendToTail node
+    unless @head
+      return @head = node
+    end
+    curr = @head
+    while curr.next
+      curr = curr.next
+    end
+    curr.next = node
+  end
+end
+
+l = LinkedList.new
+
+l.appendToTail(Node.new(1))
+l.appendToTail(Node.new(2))
+l.appendToTail(Node.new(3))
+l.appendToTail(Node.new(4))
+
+p l
